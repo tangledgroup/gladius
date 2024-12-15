8b640fec7f1c68d25b515ae65a7d22bfb60077b4
Marko Tasic <mtasic85@gmail.com>
2024-12-15 09:46:09 +0100

    removed low-level example

--- examples/pico_preview.py
+++ examples/pico_preview.py
@@ -3,52 +3,6 @@ from gladius import Gladius, Element
 g = Gladius()
 
 # home page
-home_page: Element = g.html(lang='en').add(
-    g.head().add(
-        g.meta(charset="utf-8"),
-        g.meta(name="viewport", content="width=device-width, initial_scale=1"),
-        g.meta(name="color-scheme", content="light dark"),
-        g.title().add("Preview • Pico CSS"),
-        g.meta(name="description", content="A pure HTML example, without dependencies."),
-
-        g.link(rel="stylesheet",
-               href="https://cdn.jsdelivr.net/npm/@picocss/pico@2.0.6/css/pico.min.css"),
-    ),
-    g.body().add(
-        g.header(class_="container").add(
-            g.hgroup().add(
-                g.h1().add('Pico'),
-                g.p().add('A pure HTML example, without dependencies.'),
-            )
-        ),
-        g.nav().add(
-            g.ul().add(
-                g.li().add(
-                    g.details(class_="dropdown").add(
-                        g.summary(role="button", class_="secondary").add('Theme'),
-                        g.ul().add(
-                            g.li().add(
-                                a0 := g.a(href="#", data_theme_switcher="auto").add('Auto'),
-                                a1 := g.a(href="#", data_theme_switcher="light").add('Light'),
-                                a2 := g.a(href="#", data_theme_switcher="dark").add('Dark'),
-                            )
-                        )
-                    )
-                )
-            )
-        ),
-    )
-)
-
-print(home_page)
-print(a0)
-print(a1)
-print(a2)
-print(home_page.ctx)
-print(a0.ctx)
-print(a1.ctx)
-print(a2.ctx)
-
 with g.html(lang='en') as home_page:
     with g.head():
         g.meta(charset="utf-8")
@@ -78,10 +32,10 @@ with g.html(lang='en') as home_page:
                                 a1 = g.a(href="#", data_theme_switcher="light").add('Light')
                                 a2 = g.a(href="#", data_theme_switcher="dark").add('Dark')
 
-    print(home_page)
-    print(a0)
-    print(a1)
-    print(a2)
+print(home_page)
+print(a0)
+print(a1)
+print(a2)
 
 # # router
 # g.route('/', home_page)
