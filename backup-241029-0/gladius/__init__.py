f892323c304dff7634ee91faa7caf0656374d199
Marko Tasic <mtasic85@gmail.com>
2025-01-30 21:56:02 +0100

    examples: demo_2, based on alpine

--- backup-241029-0/gladius/__init__.py
+++
@@ -1,2 +0,0 @@
-from .gladius import *
-from .component import *
---
+++ examples/demo_2/app.py
@@ -0,0 +1,25 @@
+from aiohttp import web
+from gladius.starter import create_aiohttp_app
+
+# required npm packages
+npm_packages = {
+    '@picocss/pico': ['css/pico.css'],
+    'nprogress': ['nprogress.js', 'nprogress.css'],
+}
+
+# create simple aiohttp web server
+g, page, app = create_aiohttp_app(
+    npm_packages=npm_packages, # type: ignore
+    ready='client_app.py',
+)
+
+# server-side structure
+with page:
+    with g.body(x_data=None):
+        with g.main(class_='container'):
+            g.h1('Gladius Demo')
+            g.button('Click me!', id='hello-button')    # create button on server
+
+# start application
+if __name__ == '__main__':
+    web.run_app(app, host='0.0.0.0', port=5000)
