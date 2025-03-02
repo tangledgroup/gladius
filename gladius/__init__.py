8a1d5ef3cdbe8f0c16e0d29301869b61d4dd90d2
Marko Tasic <mtasic85@gmail.com>
2025-03-02 14:31:27 +0100

    - Reload server-side Python app if client-side Python file gets changed. This method uses `gunicorn`.   - Directly import client-side Python modules in server-side. This method uses `mock_module.py`.

--- gladius/__init__.py
+++ gladius/__init__.py
@@ -1,4 +1,6 @@
 import sys
+
+# server-side uses this to handle changes of client-side Python code and reload app
 from . import mock_module
 sys.modules['browser'] = mock_module
 sys.modules['javascript'] = mock_module
